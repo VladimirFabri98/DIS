6bd8629ff7ab1cd6c2f9de095b6882c2d140e28d
VladimirFabri98 <vfficial98@gmail.com>
2023-07-31 11:50:24 +0200

    Initial gradle build executed

--- microservices/ccComposite-service/src/main/java/vladimir/microservices/core/cConversionComposite/pojo/ConversionComposite.java
+++ microservices/ccComposite-service/src/main/java/vladimir/microservices/core/cConversionComposite/pojo/ConversionComposite.java
@@ -1,5 +1,58 @@
 package vladimir.microservices.core.cConversionComposite.pojo;
 
+import vladimir.microservices.core.bankAccount.pojo.BankAccount;
+import vladimir.microservices.core.cConversion.pojo.CurrencyConversion;
+
 public class ConversionComposite {
+
+	private int compositeId;
+	private CurrencyConversion conversion;
+	private BankAccount account;
+	private String conversionInfo;
+
+	public ConversionComposite() {
+
+	}
+
+	public ConversionComposite(int compositeId, CurrencyConversion conversion, BankAccount account,
+			String conversionInfo) {
+		super();
+		this.compositeId = compositeId;
+		this.conversion = conversion;
+		this.account = account;
+		this.conversionInfo = conversionInfo;
+	}
+
+	public int getCompositeId() {
+		return compositeId;
+	}
+
+	public void setCompositeId(int compositeId) {
+		this.compositeId = compositeId;
+	}
+
+	public CurrencyConversion getConversion() {
+		return conversion;
+	}
+
+	public void setConversion(CurrencyConversion conversion) {
+		this.conversion = conversion;
+	}
+
+	public BankAccount getAccount() {
+		return account;
+	}
+
+	public void setAccount(BankAccount account) {
+		this.account = account;
+	}
+
+	public String getConversionInfo() {
+		return conversionInfo;
+	}
+
+	public void setConversionInfo(String conversionInfo) {
+		this.conversionInfo = conversionInfo;
+	}
 
 }
